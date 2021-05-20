f28fc47b92037c9dad0bdb28c2f47759a7417a3e
Logan-Seabolt <Seaboltl@ram.colostate.edu>
2021-05-20 12:34:00 -0600

    Code Update

--- main.py
+++ main.py
@@ -1,51 +1,58 @@
-import tkinter
+import PySimpleGUI as sg
+import brainflow as bf
+import keyboard as kb
+import random as rd
 
-class experiment_Program:
+class ExperimentProgram:
+
+    index = 'f'
+    middle = 'd'
+    ring = 's'
+    pinky = 'a'
+    thumb = ' '
+    test_length = 10
 
     def leftHandMap(self):
-        return {
-            "index": "f",
-            "middle": "d",
-            "ring": "s",
-            "pinky": "a",
-            "thumb": "space"
-        }
+        self.index = 'f'
+        self.middle = 'd'
+        self.ring = 's'
+        self.pinky = 'a'
+        self.thumb = ' '
 
     def rightHandMap(self):
-        return {
-            "index": "j",
-            "middle": "k",
-            "ring": "l",
-            "pinky": ";",
-            "thumb": "space"
-        }
+        self.index = 'j'
+        self.middle = 'k'
+        self.ring = 'l'
+        self.pinky = ';'
+        self.thumb = ' '
 
-    def testClick(self):
-        print("Henlo")
+    def set_test_length(self,new_test_length):
+        self.test_length=new_test_length
 
-    def runExperiment(self):
-        top = tkinter.Tk()
-        greeting = tkinter.Label(
-            text="Hello\nWelcome To Logan Seabolt\'s brain mapping experiment",
-            fg="blue",
-            bg="tan",
-            width=100,
-            height=50)
-        button = tkinter.Button(
-            text="Press to continue",
-            width=25,
-            height=5,
-            bg="silver",
-            fg="black",
-            command=self.testClick
-        )
-        greeting.pack()
-        button.pack()
-        top.mainloop()
+    def run_battery(self, order):
+        for finger in order:
+            print("Please press: \"", finger,"\"")
+            while True:
+                if kb.is_pressed(finger):
+                    print("Record Data")
+                    break
+        print("Battery Complete")
+
+    def generate_battery(self):
+        order = [self.index, self.middle, self.ring, self.pinky, self.thumb]
+        battery = []
+        for i in range(self.test_length):
+            test = rd.sample(order, len(order))
+            battery.append(test)
+        return battery
 
 
 if __name__ == '__main__':
-    exp = experiment_Program()
-    exp.runExperiment()
+    exp = ExperimentProgram()
+    battery = exp.generate_battery()
+    print(battery)
+    for order in battery:
+        exp.run_battery(order)
+
 
 
