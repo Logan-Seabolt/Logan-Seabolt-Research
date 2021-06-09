d9201565cd2c8116a5c2382814f294fb44f5700c
Logan-Seabolt <Seaboltl@ram.colostate.edu>
2021-06-09 14:06:04 -0600

    Updated some code and found some test code online

--- Testzone.py
+++ Testzone.py
@@ -1,13 +1,14 @@
 import argparse
 import time
 import numpy as np
+import pandas as pd
 
 import brainflow
-from brainflow.board_shim import BoardShim, BrainFlowInputParams
+from brainflow.board_shim import BoardShim, BrainFlowInputParams, LogLevels, BoardIds
 from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
 
 
-def main():
+def test_1():
     BoardShim.enable_dev_board_logger()
 
     parser = argparse.ArgumentParser()
@@ -53,5 +54,51 @@ def main():
     print(data)
 
 
+def test_2():
+    BoardShim.enable_dev_board_logger()
+
+    # use synthetic board for demo
+    params = BrainFlowInputParams()
+    board = BoardShim(BoardIds.SYNTHETIC_BOARD.value, params)
+    board.prepare_session()
+    board.start_stream()
+    start = time.time()
+    BoardShim.log_message(LogLevels.LEVEL_INFO.value, 'start sleeping in the main thread')
+    time.sleep(1.5)
+    data = board.get_board_data()
+    print("This measurement took:", time.time() - start, "seconds")
+    board.stop_stream()
+    #board.release_session()
+
+    # demo how to convert it to pandas DF and plot data
+    eeg_channels = BoardShim.get_eeg_channels(BoardIds.SYNTHETIC_BOARD.value)
+    df = pd.DataFrame(np.transpose(data))
+    print('Data From the Board')
+    print(df.head(10))
+
+    # demo for data serialization using brainflow API, we recommend to use it instead pandas.to_csv()
+    DataFilter.write_file(data, 'test.csv', 'w')  # use 'a' for append mode
+
+    # Collect second set of data
+    board.start_stream()
+    start = time.time()
+    BoardShim.log_message(LogLevels.LEVEL_INFO.value, 'start sleeping in the main thread')
+    for i in range(5):
+        board.insert_marker(i+1)
+        time.sleep(0.5)
+        board.insert_marker(i+1)
+    data2 = board.get_board_data()
+    print("This measurement took:", time.time() - start, "seconds")
+    board.stop_stream()
+    board.release_session()
+
+    DataFilter.write_file(data2, 'test2.csv', 'w')  # use 'a' for append mode
+
+    restored_data = DataFilter.read_file('test.csv')
+    restored_df = pd.DataFrame(np.transpose(restored_data))
+    print('Data From the File')
+    print(restored_df.head(10))
+
+
 if __name__ == "__main__":
-    main()
+    test_2()
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class ExperimentProgram:
     pinky = 'a'
     thumb = ' '
     test_length = 10
+    board = 0
 
     def left_hand_map(self):
         self.index = 'f'
@@ -29,20 +30,34 @@ class ExperimentProgram:
         self.pinky = ';'
         self.thumb = ' '
 
+    def __init__(self):
+        # Board type can be hard set to 1
+        board_id = 1
+        # Com port needs to be set on a by device basis
+        params = BrainFlowInputParams()
+        params.serial_port = "COM3"
+        self.board = BoardShim(board_id, params)
+        self.board.prepare_session()
+
     def set_test_length(self, new_test_length):
         self.test_length = new_test_length
 
     def run_battery(self, order):
+        self.board.start_stream(4500)
         for finger in order:
-            print("Please press: \"", finger,"\"")
+            fing = finger[0]
+            print("Please press: \"", fing, "\"")
+            self.board.insert_marker(finger[1])
             while True:
-                if kb.is_pressed(finger):
-                    print("Record Data")
+                if kb.is_pressed(fing):
+                    self.board.insert_marker(finger[1])
                     break
-        print("Battery Complete")
+        data = self.board.get_board_data()
+        self.board.stop_stream()
+        return battery
 
     def generate_battery(self):
-        order = [self.index, self.middle, self.ring, self.pinky, self.thumb]
+        order = [(self.index, 1), (self.middle, 2), (self.ring, 3), (self.pinky, 4), (self.thumb, 5)]
         battery = []
         for i in range(self.test_length):
             test = rd.sample(order, len(order))
@@ -54,8 +69,9 @@ if __name__ == '__main__':
     exp = ExperimentProgram()
     battery = exp.generate_battery()
     print(battery)
+    res = []
     for order in battery:
-        exp.run_battery(order)
+        res.append(exp.run_battery(order))
 
 
 
