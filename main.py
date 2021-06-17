801c682a9cabe0943f82472b16a65dafe4c5bb27
Logan-Seabolt <Seaboltl@ram.colostate.edu>
2021-06-17 14:46:22 -0600

    Last few updates To do: - ask user for how long of a break to give - give a break every 5 tests

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ class ExperimentProgram:
     thumb = ' '
     test_length = 2
     board = 0
+    response_time = 0
 
     def left_hand_map(self):
         self.index = 'f'
@@ -32,6 +33,9 @@ class ExperimentProgram:
         self.pinky = ';'
         self.thumb = ' '
 
+    def calc_response_time(self):
+        self.response_time = self.response_time/(self.test_length*5)
+
     def __init__(self):
         # Board type can be hard set to 1
         board_id = 1
@@ -41,7 +45,7 @@ class ExperimentProgram:
         #self.board = BoardShim(board_id, params)
         self.board = BoardShim(BoardIds.SYNTHETIC_BOARD.value, params)
         self.board.prepare_session()
-        time.sleep(5)
+        time.sleep(2)
 
     def set_test_length(self, new_test_length):
         self.test_length = new_test_length
@@ -50,30 +54,74 @@ class ExperimentProgram:
         self.board.start_stream(4500)
         for finger in order:
             fing = finger[0]
+            start = time.time()
             print("Please press: \"", fing, "\"")
             self.board.insert_marker(finger[1])
             while True:
                 if kb.is_pressed(fing):
                     self.board.insert_marker(finger[1])
                     break
+            self.response_time += time.time()-start
         data = self.board.get_board_data()
         self.board.stop_stream()
         return data
 
-    def generate_battery(self):
+    def run_battery_practice(self, order):
+        for finger in order:
+            fing = finger[0]
+            print("Please press: \"", fing, "\"")
+            while True:
+                if kb.is_pressed(fing):
+                    break
+
+    def run_battery_imagined(self, order):
+        self.board.start_stream(4500)
+        for finger in order:
+            fing = finger[0]
+            print("Please imagine pressing: \"", fing, "\"")
+            self.board.insert_marker(finger[1])
+            time.sleep(self.response_time)
+            self.board.insert_marker(finger[1])
+        data = self.board.get_board_data()
+        self.board.stop_stream()
+        return data
+
+    def run_battery_imagined_practice(self, order):
+        for finger in order:
+            fing = finger[0]
+            print("Please imagine pressing: \"", fing, "\"")
+            time.sleep(self.response_time)
+
+    def generate_battery(self, tlength=test_length):
         order = [(self.index, 1), (self.middle, 2), (self.ring, 3), (self.pinky, 4), (self.thumb, 5)]
         battery = []
-        for i in range(self.test_length):
+        for i in range(tlength):
             test = rd.sample(order, len(order))
             battery.append(test)
         return battery
 
 
 if __name__ == '__main__':
+    debug = False
     exp = ExperimentProgram()
-    battery = exp.generate_battery()
-    print(battery)
     res = []
+    userID = input("Enter user ID number: ")
+    exp.test_length = int(input("Enter number of experiments (15,30): "))
+    handedness = input("Are you left or right handed? l/r? ")
+    if handedness[0] == 'l':
+        exp.left_hand_map()
+    else:
+        exp.right_hand_map()
+    battery = exp.generate_battery()
+    tbattery = exp.generate_battery(5)
+    if debug:
+        print(battery)
+    while True:
+        if input("Would you like to practice the imagined movements? y/n: ")[0] == "y":
+            for bat in tbattery:
+                exp.run_battery_practice(bat)
+        else:
+            break
     print("When you are ready to begin press the space bar!")
     while True:
         if kb.is_pressed(" "):
@@ -82,7 +130,30 @@ if __name__ == '__main__':
     for order in battery:
         res.append(exp.run_battery(order))
     for data in res:
-        DataFilter.write_file(data, 'output.csv', 'a')
+        DataFilter.write_file(data, userID+'_real_movement_output.csv', 'a')
+    print("\n\nPreparing for imagined finger movement test")
+    exp.calc_response_time()
+    if debug:
+        print("Average response time", exp.response_time, "seconds")
+    time.sleep(60*5) #enforce a 5 minute break
+    while True:
+        if input("Would you like to practice the imagined movements? y/n: ")[0] == "y":
+            for bat in tbattery:
+                exp.run_battery_imagined_practice(bat)
+        else:
+            break
+    print("When you are ready to begin press the space bar!")
+    while True:
+        if kb.is_pressed(" "):
+            break
+    time.sleep(0.5)
+    res.clear()
+    battery = exp.generate_battery()
+    for order in battery:
+        res.append(exp.run_battery_imagined(order))
+    for data in res:
+        DataFilter.write_file(data, userID+'_imagined_movement_output.csv', 'a')
+
 
 
 
